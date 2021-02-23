252a7c25c71bf599226c92310c8ec17ee2ccb141
Ahmed Osama <ahmedosamabedawy@gmail.com>
2021-02-24 00:57:42 +0200

    call back-end and set data to state

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import { axios } from 'axios';
+import axios from 'axios';
 import { toast, ToastContainer } from 'react-toastify';
 import Navbar from './components/navbar/Navbar';
 
 //Import Components
 
 class App extends Component {
-  state = {  }
+  state = { 
+    products: []
+   };
+
+   //Call Data from API
+   async componentDidMount() {
+     //Call Back-end
+     const { data } = await axios.get("http://localhost:3000/ProductsDetails/")
+    //  console.log(data)
+
+    //Set State
+    this.setState({products: data})
+   }
+
   render() { 
     return ( 
       <React.Fragment>
@@ -18,5 +31,4 @@ class App extends Component {
      );
   }
 }
- 
 export default App;
